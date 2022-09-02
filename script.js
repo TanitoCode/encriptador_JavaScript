7ec5c6d104019bc7f07d227d88b10cd65e43fcba
Cristian Sanchez <mariocrsanchez@gmail.com>
2022-09-02 18:56:15 -0300

    Creando funcion encriptar en script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const inputTexto = document.querySelector(".input-texto")
+const mensaje = document.querySelector(".mensaje")
+
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["a","enter"],["e","imes"],["i","imes"],["o","ober"],["u","ufat"]]
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++ ){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada
+}
